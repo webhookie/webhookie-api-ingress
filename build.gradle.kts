a3581993ba8f41a37b215333d29a963293a7478e
Arthur Kazemi <bidadh@gmail.com>
2022-08-06 17:36:41 +1000

    gradle build script improvement

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,15 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
-  id("org.springframework.boot") version "2.7.2"
-  id("io.spring.dependency-management") version "1.0.12.RELEASE"
   kotlin("jvm") version "1.6.21"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.allopen") version "1.6.21"
   kotlin("kapt") version "1.6.21"
 }
 
-kapt.includeCompileClasspath = false
-
-allOpen {
-  annotation("com.webhookie.common.annotation.Open")
-  // annotations("com.another.Annotation", "com.third.Annotation")
-}
-
-group = "com.webhookie"
-version = "2.0.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
 configurations {
   compileOnly {
     extendsFrom(configurations.annotationProcessor.get())
   }
 }
-
-repositories {
-  mavenCentral()
-}
-
-extra["springCloudVersion"] = "2021.0.3"
 
 dependencies {
   implementation(project(":webhookie-spring-boot-starter-service"))
@@ -48,44 +25,4 @@ dependencyManagement {
   imports {
     mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
   }
-}
-
-tasks.withType<KotlinCompile> {
-  kotlinOptions {
-    freeCompilerArgs = listOf("-Xjsr305=strict")
-    jvmTarget = "17"
-  }
-}
-
-tasks.withType<Test> {
-  useJUnitPlatform()
-}
-
-tasks.named<BootJar>("bootJar") {
-  layered {
-    application {
-      intoLayer("spring-boot-loader") {
-        include("org/springframework/boot/loader/**")
-      }
-      intoLayer("application")
-    }
-    dependencies {
-      intoLayer("snapshot-dependencies") {
-        include("*:*:*SNAPSHOT")
-      }
-      intoLayer("internal-dependencies") {
-        include("com.webhookie:*:*")
-      }
-      intoLayer("dependencies")
-    }
-    layerOrder = listOf("dependencies", "spring-boot-loader", "internal-dependencies", "snapshot-dependencies", "application")
-  }
-}
-
-tasks.named<BootBuildImage>("bootBuildImage") {
-  imageName = "webhookie.com/services/${project.name.replace("webhookie-", "")}"
-}
-
-springBoot {
-  buildInfo()
 }
